0b3dd8f81846a9c0f7c5d8eb8b2039988a5a8968
ndmaxwell <nicholasmaxwell@Computer-777.local>
2020-11-18 22:50:41 -0600

    More updates

--- shinyappdata/app.R
+++ shinyappdata/app.R
@@ -10,11 +10,18 @@
 library(shiny)
 library(tidyverse)
 library(shinythemes)
-library(gganimate)
+library(tidymodels)
+library(rstan)
 justiceCenteredfixed <- readRDS("justice.rds")
+roberts_ideology_decisions <- readRDS("roberts.rds")
 
-justiceslist <- justiceCenteredfixed %>% 
-    select(justiceName)
+justicelist <- roberts_ideology_decisions %>% 
+    select(justiceName) %>% 
+    unique()
+
+issuelist <- roberts_ideology_decisions %>% 
+    select(issueName) %>% 
+    unique()
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(theme = shinytheme("flatly"), navbarPage(
@@ -35,7 +42,7 @@ ui <- fluidPage(theme = shinytheme("flatly"), navbarPage(
                 selectInput(
                     inputId = "Justice",                 # a name for the value you choose here
                     label = "Look at the Change in a Particular Justice's Ideology",
-                    choices = justiceslist)                       # your list of choices to choose from
+                    choices = justicelist)                       # your list of choices to choose from
         ),
 
         # Show a plot of the generated distribution
@@ -48,12 +55,29 @@ ui <- fluidPage(theme = shinytheme("flatly"), navbarPage(
 
 tabPanel("About",
          h3("This is an about me! My name is Nick Maxwell"),
-         p("Here is the link to my Github: https://github.com/ndmaxwell/milestone4"),
+         p("Here is the link to my Github: https://github.com/ndmaxwell/finalproject"),
          p("For this project, I'm pulling data from several sources related to the Supreme Court.
                One set contains the full text from nearl every Supreme Court decision, and another
            records decisions by Justices as defined by ideology. Finally, I have a dataset that makes 
            use of more personal background information on the Justices. Collectively, I intend to use these
-           datasets to explore the ideological evolutions of different Justices based on their personal characteristics."))))
+           datasets to explore the ideological evolutions of different Justices based on their personal characteristics."))),
+
+tabPanel("Predicting A Justice's Vote",
+         sidebarLayout(
+         selectInput(
+             inputId = "Justice",
+             label = "Select a Justice from the Roberts Court",
+             choices = justicelist),
+         selectInput(
+             inputId = "Issue",
+             label = "Select an Issue",
+             choices = issuelist) 
+         ),
+         mainPanel(
+             plotOutput("predictionplot")
+         )))
+
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -81,6 +105,18 @@ server <- function(input, output) {
                  x = "Year",
                  title = "Ideology of Justice Over Time")
     })
+    
+    output$predictionplot <- renderPlot({
+        roberts_ideology_decisions %>% 
+            filter(justiceName == input$Justice) %>%
+            ggplot(aes(x = year, y = ideology)) +
+            geom_line() +
+            theme_classic() +
+            labs(y = "Ideology Score - Conservative (1) - Liberal (2)",
+                 x = "Year",
+                 title = "Ideology of Justice Over Time")
+    })
+    
 }
 
 # Run the application 
