43e19cce808fe69b416c93c1c3e2991a7c262e45
ndmaxwell <nicholasmaxwell@Computer-777.local>
2020-11-20 21:08:44 -0600

    changed 11/20 - got model working with kevin

--- shinyappdata/app.R
+++ shinyappdata/app.R
@@ -11,17 +11,18 @@ library(shiny)
 library(tidyverse)
 library(shinythemes)
 library(tidymodels)
-library(rstan)
+library(rstanarm)
 justiceCenteredfixed <- readRDS("justice.rds")
 roberts_ideology_decisions <- readRDS("roberts.rds")
 
-justicelist <- roberts_ideology_decisions %>% 
-    select(justiceName) %>% 
-    unique()
+justicelist <- unique(roberts_ideology_decisions$justiceName)
 
-issuelist <- roberts_ideology_decisions %>% 
-    select(issueName) %>% 
-    unique()
+issuelist <- unique(roberts_ideology_decisions$issueArea)
+
+prediction_model <- stan_glm(data = roberts_ideology_decisions,
+                             formula = conservative ~ post_mn + issueArea,
+                             refresh = 0,
+                             family = binomial(link = "logit"))
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(theme = shinytheme("flatly"), navbarPage(
@@ -40,9 +41,9 @@ ui <- fluidPage(theme = shinytheme("flatly"), navbarPage(
                         max = 2020,
                         value = 1),
                 selectInput(
-                    inputId = "Justice",                 # a name for the value you choose here
+                    inputId = "Justice",    # a name for the value you choose here
                     label = "Look at the Change in a Particular Justice's Ideology",
-                    choices = justicelist)                       # your list of choices to choose from
+                    choices = justicelist)   # your list of choices to choose from
         ),
 
         # Show a plot of the generated distribution
@@ -60,24 +61,20 @@ tabPanel("About",
                One set contains the full text from nearl every Supreme Court decision, and another
            records decisions by Justices as defined by ideology. Finally, I have a dataset that makes 
            use of more personal background information on the Justices. Collectively, I intend to use these
-           datasets to explore the ideological evolutions of different Justices based on their personal characteristics."))),
+           datasets to explore the ideological evolutions of different Justices based on their personal characteristics.")),
 
-tabPanel("Predicting A Justice's Vote",
+tabPanel("Predicting A Convervative Vote by Issue",
          sidebarLayout(
-         selectInput(
-             inputId = "Justice",
-             label = "Select a Justice from the Roberts Court",
-             choices = justicelist),
-         selectInput(
-             inputId = "Issue",
-             label = "Select an Issue",
-             choices = issuelist) 
+             sidebarPanel(
+              selectInput(
+               inputId = "Issue",
+               label = "Select an Issue",
+               choices = issuelist), 
+              
          ),
          mainPanel(
              plotOutput("predictionplot")
-         )))
-
-
+         )))))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -105,19 +102,42 @@ server <- function(input, output) {
                  x = "Year",
                  title = "Ideology of Justice Over Time")
     })
-    
+
     output$predictionplot <- renderPlot({
-        roberts_ideology_decisions %>% 
-            
-            filter(justiceName == input$Justice) %>%
-            ggplot(aes(x = year, y = ideology)) +
+        
+        prediction_data <- tibble("post_mn" = seq(-4, 4, 0.1),
+                                  "issueArea" = input$Issue)
+        prediction_posterior <- posterior_predict(
+            prediction_model, newdata = prediction_data, draws = 4000) %>%
+            as_tibble() %>%
+            mutate_all(as.numeric) %>% 
+            pivot_longer(cols = everything(),
+                         names_to = "ideology_score",
+                         values_to = "outcome") %>% 
+            mutate(ideology_score = as.numeric(ideology_score)) %>%
+            group_by(ideology_score) %>% 
+            summarize(proportion_voting = mean(outcome), .groups = "drop") %>% 
+            mutate(ideology_score = -4 + .1*(ideology_score - 1))
+        
+        prediction_posterior %>% 
+            ggplot(aes(x = ideology_score, y = proportion_voting)) +
             geom_line() +
-            theme_classic() +
-            labs(y = "Ideology Score - Conservative (1) - Liberal (2)",
-                 x = "Year",
-                 title = "Ideology of Justice Over Time")
+            labs(title = "Posterior Distribution for Probability of Voting Conservative",
+                 x = "Ideology",
+                 y = "Probability") +
+            scale_x_continuous(breaks = seq(-4, 4, 1)) +
+            theme_linedraw()
     })
     
+output$ACBplot <- renderPlot({   
+    new <- tibble("post_mn" = input$Ideology,
+                  "issueArea" = input$Issue_2)
+    
+    prediction_posterior <- posterior_predict(prediction_model, newdata = new) %>%
+        as_tibble() %>%
+        mutate_all(as.numeric) %>% 
+        summarize(prob.conservative = mean(`1`))
+  })
 }
 
 # Run the application 
