307cdbc484767b35f3db62469d0b1bb82019141b
ndmaxwell <nicholasmaxwell@Computer-777.local>
2020-11-24 23:10:27 -0600

    Took out model from file. More efficient after simply importing Tibble.

--- shinyappdata/app.R
+++ shinyappdata/app.R
@@ -1,21 +1,17 @@
-
 library(shiny)
 library(tidyverse)
 library(shinythemes)
-library(tidymodels)
 library(rstanarm)
+library(RColorBrewer)
+
 posterior_all_issue_ideology <- readRDS("posterior_all_issue_ideology.rds")
 roberts_ideology_decisions <- readRDS("roberts.rds")
 roberts_issues <- readRDS("robertsissues.rds")
-justicelist <- unique(roberts_ideology_decisions$justiceName)
-justicelistnooconnor <- justicelist[-2]
 
+justicelist <- unique(roberts_ideology_decisions$justiceName)
+justicelist_no_oconnor <- justicelist[-2]
 issuelist <- unique(roberts_ideology_decisions$issueArea)
 
-prediction_model <- stan_glm(data = roberts_ideology_decisions,
-                             formula = conservative ~ post_mn * issueArea,
-                             refresh = 0,
-                             family = binomial(link = "logit"))
 
 ui <- fluidPage(theme = shinytheme("flatly"), navbarPage(
     "Examining the Ideology of the Roberts Court",
@@ -26,8 +22,8 @@ ui <- fluidPage(theme = shinytheme("flatly"), navbarPage(
            better sense of how maleable justices' ideologies were over time. 
            Second, I wanted to see to what extent we would be able to predict a 
            justice's vote on a given issue area given their ideological score 
-           (specifically, their Martin-Quinn score). To build this model and find 
-           the historical data necessary, I combined data first from Washington 
+           (specifically, their Martin-Quinn score). To build this model and inform my question, 
+           I first looked at date from the Washington 
            University Law's Supreme Court", a("database", href="http://scdb.wustl.edu/data.php")),
            p("This historical data, combined with the University of Michigan's Justices dataset,
              allowed me to examine historical trends in the Roberts court and make
@@ -42,18 +38,18 @@ ui <- fluidPage(theme = shinytheme("flatly"), navbarPage(
                 checkboxGroupInput(
                     inputId = "Justices", 
                     label = "Look at the Change of in the Ideologies of Justices over Time",
-                    choices = justicelistnooconnor) 
-                
-                # Here, Justice O'Connor is excluded because of her short tenure
-                # on the court. Because we only have data on decisions from
-                # 2005, it would not make sense to represent the change in her
-                # ideology.
+                    choices = justicelist_no_oconnor) 
         ),
+        
         mainPanel(
-           plotOutput("justiceplotselect")
+           plotOutput("justiceplotselect"),
+      
+        p("Justice O'Connor is excluded from the choices in justies for this plot, 
+          as she retired from the Court shortly after Chief Justice Roberts joined.
+          Because of this, it wouldn't make much sense to examine changes in her 
+          ideology for such a short duration.")),
         )
-    )
-),
+    ),
 
 tabPanel("How has the Roberts Court Voted on Different Issues?",
          sidebarLayout(
@@ -82,7 +78,6 @@ tabPanel("Predicting A Convervative Vote by Issue",
 ))
 
 
-
 server <- function(input, output) {
     
   output$justiceplotselect <- renderPlot({
@@ -102,26 +97,26 @@ server <- function(input, output) {
   output$issueplot <- renderPlot({   
     roberts_issues %>% 
       filter(issueArea == input$Issue) %>% 
-      ggplot(aes(y = fct_reorder(justiceName, percentage), x = percentage)) +
-      geom_col() +
+      ggplot(aes(y = fct_reorder(justiceName, percentage), x = percentage,
+                 fill = justiceName)) +
+      geom_col(show.legend = F) +
       labs(x = "Proportion of Conservative Votes",
            y = "Justice",
            title = "Examining Conservative Vote Distribution by Issue",
-           subtitle = "Conservatism isn't the same across the board")
-    
+           subtitle = "Conservatism isn't the same across the board") +
+      scale_fill_manual(values = c(rep("dodgerblue4", 14)))
   })
   
     output$predictionplot <- renderPlot({
       posterior_all_issue_ideology %>% 
         filter(issueArea == input$Issue_2) %>% 
         ggplot(aes(x = post_mn, y = outcome)) +
-         geom_line() +
+         geom_line(color = "dodgerblue4") +
          labs(title = "Posterior Distribution for Probability of Voting Conservative",
               x = "Ideology (Higher Scores are more Conservative)",
              y = "Probability") +
          scale_x_continuous(breaks = seq(-4, 4, 1)) +
           theme_linedraw()
-
     })
 
 }
